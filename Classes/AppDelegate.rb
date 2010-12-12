4aa757630767026634189db62d3e6bfbb0b3f72f
Andrew Sardone <asardone@nutshell.com>
2010-12-11 21:24:00 -0500

    Created and set up the dummy app delegate

---
+++ Classes/AppDelegate.rb
@@ -0,0 +1,13 @@
+# AppDelegate.rb
+# Ralph
+#
+# Created by Andrew Sardone on 12/11/10.
+# Copyright 2010 __MyCompanyName__. All rights reserved.
+
+class AppDelegate
+
+  def applicationDidFinishLaunching(notification)
+    puts "application did finish launching"
+  end
+
+end
