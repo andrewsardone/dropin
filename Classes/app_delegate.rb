14d043ce77c6438345347e5e25ee7a58e04b478e
Andrew Sardone <asardone@nutshell.com>
2010-12-12 10:18:06 -0500

    app delegate file named with underscore (Ruby convention)
    I've been looking over some other MacRuby projects in the community,
    and it seems like people stick to Ruby conventions when they can.

--- Classes/app_delegate.rb
+++ Classes/app_delegate.rb
@@ -1,10 +1,13 @@
-# AppDelegate.rb
+# app_delegate.rb
 # Ralph
 #
 # Created by Andrew Sardone on 12/11/10.
 # Copyright 2010 __MyCompanyName__. All rights reserved.
 
+
 class AppDelegate
+
+  attr_accessor :status_item_controller
 
   def applicationDidFinishLaunching(notification)
     puts "application did finish launching"
