8af0c2f512de87677f0ce4575fe14aab688ba67e
Andrew Sardone <asardone@nutshell.com>
2010-12-12 21:46:23 -0500

    First stab at implementing NSDraggingDestination protocol
    Just draggingEntered: implementation

--- Classes/Views/status_item_view.rb
+++ Classes/Views/status_item_view.rb
@@ -1,3 +1,25 @@
 class StatusItemView < NSView
 
+  ### Object Life Cycle
+
+  def initWithFrame(frame)
+    if super
+        self.registerForDraggedTypes [ NSFilenamesPboardType ]
+    end
+    self
+  end
+  
+  ### NSDraggingDestination methods
+  
+  def draggingEntered(sender)
+    sourceDragMask = sender.draggingSourceOperationMask
+    pboard = sender.draggingPasteboard
+    
+    if pboard.types.include? NSFilenamesPboardType
+      return NSDragOperationCopy if sourceDragMask & NSDragOperationCopy
+    end
+    
+    return NSDragOperationNone
+  end
+
 end
