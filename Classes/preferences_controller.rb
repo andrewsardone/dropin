8c8a0c481022a6eb8920fd95e1104d1813cd53db
Andrew Sardone <asardone@nutshell.com>
2010-12-12 20:05:46 -0500

    prefs controller uses an NSviewController for its contentView
    The NSViewController, PreferencesContentViewController, is also defined
    in the same file. We'll keep it simple like this until the preferences
    window proves to need a toolbar.
    
    Also moving initialization of PreferencesController out of class factory
    method and into init method.

--- Classes/preferences_controller.rb
+++ Classes/preferences_controller.rb
@@ -5,20 +5,66 @@
 # Copyright 2010 __MyCompanyName__. All rights reserved.
 
 
+PREFS_SIZE = [400, 300]
+
+class PreferencesContentViewController < NSViewController
+
+  ### Object Life Cycle
+  
+  def initWithNibName(nibNameOrNil, bundle:nibBundleOrNil)
+    if super
+      self.view = NSView.alloc.initWithFrame [49, 21] + PREFS_SIZE
+      
+      @start_at_login_button = NSButton.alloc.initWithFrame [0, 0, self.view.frame.size.width, self.view.frame.size.height]
+      @start_at_login_button.title = NSBundle.localized_string_for_key("DIPreferencesButtonStartAtLogin")
+      @start_at_login_button.buttonType = NSSwitchButton
+      @start_at_login_button.bezelStyle = NSRegularSquareBezelStyle
+      @start_at_login_button.target = self
+      @start_at_login_button.action = "start_at_login_button_clicked:"
+      
+      self.view.addSubview @start_at_login_button
+    end
+    self
+  end
+  
+  ### Actions
+  
+  def start_at_login_button_clicked(sender)
+    puts "start at login button clicked"
+  end
+
+end
+
+
 class PreferencesController < NSWindowController
 
-  def self.preferences_controller
-    screen_frame = NSScreen.screens[0].visibleFrame
-    panel_width, panel_height = [400, 300]
-    panel_x = screen_frame.origin.x + (screen_frame.size.width - panel_width)/2
-    panel_y = screen_frame.origin.y + (screen_frame.size.height - panel_height)/2
-    
-    panel = NSPanel.alloc.initWithContentRect [panel_x, panel_y, panel_width, panel_height],
-                                    styleMask: NSTitledWindowMask | NSClosableWindowMask,
-                                      backing: NSBackingStoreBuffered,
-                                        defer: false
-    panel.title = NSBundle.localized_string_for_key "DIPreferencesTitle"
-    PreferencesController.alloc.initWithWindow panel
+  attr_accessor :pref_content_view_controller
+
+  ### Object Life Cycle
+
+  def init
+    if super
+      self.pref_content_view_controller = PreferencesContentViewController.alloc.initWithNibName nil, bundle:nil
+      
+      screen_frame = NSScreen.screens[0].visibleFrame
+      prefs_width, prefs_height = PREFS_SIZE
+      prefs_x = screen_frame.origin.x + (screen_frame.size.width - prefs_width) / 2
+      prefs_y = screen_frame.origin.y + (screen_frame.size.height - prefs_height) / 2
+      prefs_frame = [prefs_x, prefs_y, prefs_width, prefs_height]
+      
+      prefs_window = NSWindow.alloc.initWithContentRect prefs_frame,
+                                              styleMask:NSTitledWindowMask | NSClosableWindowMask,
+                                                backing:NSBackingStoreBuffered,
+                                                  defer:true
+      prefs_window.showsToolbarButton = false
+      prefs_window.title = NSBundle.localized_string_for_key "DIPreferencesTitle"
+      
+      prefs_window.contentView.addSubview self.pref_content_view_controller.view
+      prefs_window.initialFirstResponder = self.pref_content_view_controller.view
+      
+      self.window = prefs_window
+    end
+    self
   end
 
 end
